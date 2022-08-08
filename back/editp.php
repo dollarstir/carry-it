0fe638b458cf63143c9f648448c28550330561f1
Frederick Ennin <kpin463@gmail.com>
2022-08-08 13:54:07 +0000

    sdfsdf

--- back/editp.php
+++ back/editp.php
@@ -61,7 +61,7 @@ begin('Add Package'); ?>
                                 <div class="mb-3">
                                     <label class="form-label" for="exampleFormControlInput1">Current Location</label>
                                     <input type="text" class="form-control" id="exampleFormControlInput1"
-                                        placeholder="" name="current" value="<<?php echo $row['current']; ?>">
+                                        placeholder="" name="current" value="<?php echo $row['current']; ?>">
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label" for="exampleFormControlInput1">Destination Service Area</label>
