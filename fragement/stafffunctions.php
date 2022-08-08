c82d47e0c5e293cd564a5d0d7155440ab57184fb
Frederick Ennin <kpin463@gmail.com>
2022-08-08 11:42:13 +0000

    sdfsd

--- fragement/stafffunctions.php
+++ fragement/stafffunctions.php
@@ -174,8 +174,7 @@ function showpackage()
     $dd = fetchall('package');
     foreach ($dd as $row) {
         echo '<tr>
-        <th scope="row">  '.$row['trackid'].'
-        </th>
+        <td>'.$row['trackid'].'</td>
         <td>'.$row['item'].'</td>
         <td>'.$row['origin'].'</td>
         <td>'.$row['current'].'</td>
