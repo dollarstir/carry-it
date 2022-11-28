5f5b5761f3c27b9463e01b43f3483700114484a8
Frederick Ennin <kpin463@gmail.com>
2022-11-28 11:20:15 +0000

    46

--- back/editp.php
+++ back/editp.php
@@ -88,7 +88,7 @@ begin('Edit Package'); ?>
                                 <div class="mb-3">
                                     <label class="form-label" for="exampleFormControlInput1">Pickup Time</label>
                                     <input type="text" class="form-control" id="exampleFormControlInput1"
-                                        placeholder="" name="pickuptime">
+                                        placeholder="" name="pickuptime" value="<?php pdetail($myid, 'pickupdate'); ?>">
                                 </div>
 
                                 <div class="mb-3">
