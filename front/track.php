a349297c5dd44100270b6a086d0feba2f7794d52
Frederick Ennin <kpin463@gmail.com>
2022-11-30 14:30:36 +0000

    74

--- front/track.php
+++ front/track.php
@@ -195,7 +195,7 @@ if (isset($_GET['tracking'])) {
                     
                     <div class="bs-docs-example">
         
-                        <table class="table table-bordered table-responsive table-hover table-striped">
+                        <table class="table table-responsive table-responsive table-hover">
                         <thead style="background-color:#ffb400">
                             <tr>
                                 <th>Date</th>
